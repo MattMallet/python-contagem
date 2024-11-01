b1611ce22f5ff6b36b1be9e9af7e8506109fb479
MattMallet <mateusrmallet@gmail.com>
2024-10-31 22:30:55 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+# for i in range(10):
+#     print(i)
+
+
+# for i in range(3,10):
+#     print(i)
+
+
+#horas = 0
+# minutos = 0
+# segundos = 0
+#
+# for i in range(horas,24):
+#     for c in range(minutos,60):
+#         for p in range(segundos,60):
+#             print(f"{i:02d}:{c:02d}:{p:02d}")
+
+
+
+# lista = []
+#
+# while True:
+#     fila = input("Sair ou Entrar da FILA? (e/s): ")
+#     if fila == "e":
+#         nome = input("Digite o integrante da fila: ")
+#         lista.append(nome)
+#     else:
+#         lista.pop(0)
+#     if fila == "X":
+#         break
+#     print(f'Fila atual: {lista}
+
+
+
+# n = int(input("Digite o numero de notas: "))
+# soma = 0
+#
+# for i in range(n):
+#     valor = float(input(f"Digite a {i+1} nota: "))
+#     soma += valor
+#
+# media = soma/n
+# print(f"A media das notas e de {media}")
+
+
+# def quadrado(x):
+#     a = x**2
+#     return a
+#
+# quadrado(3)
